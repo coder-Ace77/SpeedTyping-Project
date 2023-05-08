1b49ce28189592366784fe85c2270f44f1b22662
A <mohdadil@iitbhilai.ac.in>
2023-05-08 20:11:22 +0530

    Done for merge

--- app.js
+++ app.js
@@ -4,44 +4,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const Schema =mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const text = new Schema({
-    text:String
+    text: String
 })
-const string = mongoose.model('text',text);
+const string = mongoose.model('text', text);
 app.set('view engine', 'pug');
-app.set('views', 'views');
-app.use(express.static(path.join(__dirname,'Public')));
-// const adminData = require('./routes/admin');
-const generalRoutes = require('./routes/main');
+app.use(express.static(path.join(__dirname, 'Public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',(req,res)=>{
-    res.sendFile(path.join(__dirname,"Public","input.html"));
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 });
-
-app.post('/submit',(req,res)=>{
-    const x = req.body.input;
-    console.log(x);
-    const new_text = new string({text:x});
-    new_text.save().then((result)=>{
-        console.log("Saved!!");
-        res.redirect('/admin');
-    })
-});
-app.use('/', (req,res)=>{
-    string.find().then(result=>{
-        console.log(result,result.length);
-        const no = Math.floor((Math.random())*result.length);
-        const injector = result[no].text;
-        res.render('index.pug',{text:injector});
-    })
-});
-
-mongoose.connect('mongodb+srv://mitulvardhan:mitula123@cluster0.rzs4ajh.mongodb.net/?retryWrites=true&w=majority').then(result=>{
-    console.log("Connected");
-    app.listen(5000);
-})
+// mongoose.connect('mongodb+srv://mitulvardhan:mitula123@cluster0.rzs4ajh.mongodb.net/?retryWrites=true&w=majority').then(result => {
+//     console.log("Connected");
+app.listen(5000);
